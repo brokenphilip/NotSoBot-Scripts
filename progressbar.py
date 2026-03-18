e9804f735a0bb1e057bb7f224746a95c6ff3c699
brokenphilip <13336890+brokenphilip@users.noreply.github.com>
2026-03-18 12:06:09 +0100

    oops

--- progressbar.py
+++ progressbar.py
@@ -4,4 +4,4 @@ import time
 t = datetime.datetime.fromtimestamp(time.time())
 d = t - datetime.datetime(t.year,1,1,0,0,0,0,None)
 if (t.year%4==0): print("{} is {}% complete ({}% remaining)".format(t.year,round((100/31622400)*d.total_seconds(),2),100.0-round((100/31622400)*d.total_seconds(),2)))
-else: print("{} is {}% complete ({}% remaining)".format(t.year,round((100/31536000)*d.total_seconds(),2),100.0-round((100/31622400)*d.total_seconds(),2)))}
+else: print("{} is {}% complete ({}% remaining)".format(t.year,round((100/31536000)*d.total_seconds(),2),100.0-round((100/31622400)*d.total_seconds(),2)))
