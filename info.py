41983fda1384bac622c5f761884bcec501769ab4
brokenphilip <13336890+brokenphilip@users.noreply.github.com>
2026-03-18 11:27:06 +0100

    info

---
+++ info.py
@@ -0,0 +1,26 @@
+# Info v1.0
+
+print("## User info:")
+print("**Name:** {}#{}".format(nsb_user, nsb_discrim))
+print("**ID:** {}".format(nsb_id))
+print("**Nickname:** {}".format(nsb_nick))
+print("**Avatar:** {}".format(nsb_avatar))
+
+print("## Server info:")
+print("**Name:** {}".format(nsb_server))
+print("**ID:** {}".format(nsb_serverid))
+print("**Member count:** {}".format(nsb_servercount))
+
+print("## Channel info:")
+print("**Name:** {}".format(nsb_channel))
+print("**ID:** {}".format(nsb_channelid))
+
+print("## Command info:")
+print("**Number of arguments:** {}".format(nsb_argslen))
+print("**Arguments:** {}".format(nsb_args))
+print("**Prefix:** {}".format(nsb_prefix))
+
+print("## Random:")
+print("**User:** {}".format(nsb_randuser))
+print("**Online user:** {}".format(nsb_randonline))
+print("**Channel:** {}".format(nsb_randchannel))
